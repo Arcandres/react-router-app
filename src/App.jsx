917217d5581b56058de1c599d4436ee214b463b9
Arcandres <Arcandres@gmail.com>
2024-05-28 16:09:48 -0500

    Basic SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
+import { Routes, Route, Link, useParams } from 'react-router-dom'
+
 import './App.css'
+
+const Home = () => <h2>Home</h2>
+
+const Search = () => {
+  const cats = [
+    'JavaScript',
+    'React',
+    'NodeJs',
+    'TypeScript'
+  ]
+
+  return (
+    <>
+      <h2>Search Page</h2>
+      <nav>
+        <ul>
+          {cats.map(
+            cat => (<li key={cat}><Link to={`/search/category/${cat}`}>{cat}</Link></li>)
+          )}
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+const Categories = () => {
+  const { name } = useParams()
+  return (
+    <>
+      <h2>Category</h2>
+      <p>{name}</p>
+    </>
+  )
+}
 
 function App() {
 
   return (
     <>
-      <h1>Hello world!</h1>
+      <header>
+        <h1>Learning React Router</h1>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/search'>Search</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/search/category/:name' element={<Categories />} />
+      </Routes>
     </>
   )
 }
