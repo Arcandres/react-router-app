0788254832ec44c373deee1cd9f6f925a3ca97fe
Arcandres <Arcandres@gmail.com>
2024-06-03 23:20:50 -0500

    Highlight code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { Routes, Route, Link, useParams } from 'react-router-dom'
-
+import { Routes, Route, Link, useParams, Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import hljs from 'highlight.js'
+import javascript from 'highlight.js/lib/languages/javascript';
+import 'highlight.js/styles/atom-one-dark.css'
 import './App.css'
+
+hljs.registerLanguage('javascript', javascript);
 
 const Home = () => <h2>Home</h2>
 
@@ -32,6 +37,84 @@ const Categories = () => {
     <>
       <h2>Category</h2>
       <p>{name}</p>
+      <Link to='post'>Get Post</Link>
+      <Outlet />
+    </>
+  )
+}
+
+const Post = () => {  
+  const { name } = useParams()
+    
+  const posts = [
+    {
+      title: 'JavaScript',
+      content: 
+`function getIntoAnArgument() {
+    var args = arguments.slice();
+    args.forEach(function(arg) {
+        console.log(arg);
+    });
+}`
+    },
+    {
+      title: 'React',
+      content:
+`function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Welcome name="Faisal Arkan" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);`
+    },
+    {
+      title: 'NodeJs',
+      content: 
+`var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);`
+    }, 
+    {
+      title: 'TypeScript',
+      content: 
+`interface User {
+  name: string;
+  id: number;
+}
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount("Murphy", 1);`
+    }
+  ]
+
+  const GetPostFromCat = ({ cat }) => {
+
+    const { content } = posts.find(post => post.title === cat ? post.content : null)
+
+    useEffect(() => { hljs.highlightAll() }, [])
+
+    return ( <pre><code>{content}</code></pre> )
+  }
+
+  return (
+    <>
+      <p>{name} post</p>
+      <GetPostFromCat cat={name} />
     </>
   )
 }
@@ -52,7 +135,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/search' element={<Search />} />
-        <Route path='/search/category/:name' element={<Categories />} />
+        <Route path='/search/category/:name' element={<Categories />}>
+          <Route path='post' element={<Post />} />
+        </Route>
+        <Route path='*' element={<h2>Not Found</h2>} />
       </Routes>
     </>
   )
